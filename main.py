5007556343cf31b47af70904b210c1abcbffba76
Yonatan Diga <digayona@post.bgu.ac.il>
2024-11-01 19:45:14 +0200

    added more cities to DB

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ from SaveInDB import *
 if __name__ == '__main__':
     #get_restaurant_links("https://wolt.com/he/discovery/restaurants", 'הרצליה')
 
-    rest = ['לוד', 'רמלה', 'נצרת', 'רעננה', 'ראש העין', 'קריית גת']
+    #rest = ['שדרות', 'באר יעקב', 'טירת כרמל', 'כפר יונה', 'מגדל העמק' , 'קריית מלאכי','יקנעם עילית' ,'נשר']
+    rest = ['קריית ביאליק', 'נוף הגליל' , 'קריית מוצקין', 'קריית ים','צפת','קריית שמונה']
     for r in rest:
         get_restaurant_links("https://wolt.com/he/discovery/restaurants", r)
